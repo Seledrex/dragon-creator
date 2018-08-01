2f8cf168c2ad7eec8daa248bb69f35ba2deb5d57
EAuster <EAuster@slb.com>
2018-08-01 16:56:23 -0500

    Working custom color controls

--- src/main/scala/app/ColorChooser.scala
+++ src/main/scala/app/ColorChooser.scala
@@ -5,16 +5,18 @@ package app
 //======================================================================================================================
 
 import javafx.beans.{binding => jfxb}
-import javafx.scene.{layout => jfxl, paint => jfxp, control => jfxc}
+import javafx.scene.{control => jfxc, layout => jfxl, paint => jfxp}
 import scalafx.Includes._
 import scalafx.beans.binding.Bindings
 import scalafx.beans.property.{DoubleProperty, ObjectProperty}
 import scalafx.event.ActionEvent
-import scalafx.geometry.Insets
-import scalafx.scene.control.{Button, Tooltip}
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.control.TextFormatter.Change
+import scalafx.scene.control._
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout._
 import scalafx.scene.paint.{Color, CycleMethod, LinearGradient, Stop}
+import scalafx.scene.text.Font
 
 //======================================================================================================================
 // ColorChooser
@@ -36,6 +38,9 @@ class ColorChooser extends VBox {
   private val hue = new DoubleProperty(Bean, PropName, DefaultColor.hue * 100)
   private val sat = new DoubleProperty(Bean, PropName, DefaultColor.saturation * 100)
   private val bright = new DoubleProperty(Bean, PropName, DefaultColor.brightness * 100)
+  private val red = new DoubleProperty(Bean, PropName, 255)
+  private val green = new DoubleProperty(Bean, PropName, 255)
+  private val blue = new DoubleProperty(Bean, PropName, 255)
   private val alpha = new DoubleProperty(Bean, PropName, DefaultColor.opacity * 100)
 
   private val buttonProp = new ObjectProperty[Button](Bean, PropName, null) {
@@ -162,6 +167,254 @@ class ColorChooser extends VBox {
     Tooltip.install(this, new Tooltip("Click to save color"))
   }
 
+  private val colorCodeControl: HBox = new HBox(Properties.Padding) {
+    alignment = Pos.Center
+    children = Seq(
+      new VBox(Properties.Padding) {
+        children = Seq(
+          new HBox(Properties.Padding) {
+            children = Seq(
+              new Label("R:") {
+                prefWidth = 15
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 40
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 3
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              },
+              new Label("G:") {
+                prefWidth = 15
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 40
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 3
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              },
+              new Label("B:") {
+                prefWidth = 15
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 40
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 3
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              }
+            )
+          },
+          new HBox(Properties.Padding) {
+            children = Seq(
+              new Label("H:") {
+                prefWidth = 15
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 40
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 3
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              },
+              new Label("S:") {
+                prefWidth = 15
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 40
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 3
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              },
+              new Label("V:") {
+                prefWidth = 15
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 40
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 3
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              }
+            )
+          },
+          new HBox(Properties.Padding) {
+            children = Seq(
+              new Label("Hex:") {
+                alignment = Pos.CenterRight
+              },
+              new TextField() {
+                prefWidth = 60
+                font = Font("Courier New")
+                alignmentInParent = Pos.Center
+                textFormatter = new TextFormatter[String]( {change: Change =>
+                  val maxLen: Int = 6
+                  if (change.controlNewText.length > maxLen) {
+                    change.text = ""
+                  }
+                  if (change.anchor > maxLen) change.anchor = maxLen
+                  if (change.caretPosition > maxLen) change.caretPosition = maxLen
+                  change
+                })
+              },
+              new Button("Enter") {
+                alignmentInParent = Pos.Center
+              }
+            )
+          }
+        )
+      }
+    )
+  }
+
+  private val colorControl: VBox = new VBox(Properties.Padding) {
+
+    styleClass.add("color-control")
+    alignment = Pos.Center
+
+    // Controls
+    val labels = new Array[Label](3)
+    val sliders = new Array[Slider](3)
+    val fields = new Array[TextField](3)
+
+    val settingsPane = new VBox()
+
+    // Update settings
+    for (i <- 0 until 3) {
+      labels(i) = new Label("Brightness:") {
+        prefWidth = 60
+      }
+      sliders(i) = new Slider() {
+        prefWidth = 100
+      }
+      fields(i) = new TextField() {
+        prefWidth = 40
+        font = Font("Courier New")
+        textFormatter = new TextFormatter[String]( {change: Change =>
+          val maxLen: Int = 3
+          if (change.controlNewText.length > maxLen || !change.controlNewText.matches("[0-9]+")) {
+            change.text = ""
+          }
+          if (change.anchor > maxLen) change.anchor = maxLen
+          if (change.caretPosition > maxLen) change.caretPosition = maxLen
+          change
+        })
+      }
+      settingsPane.children.add(
+        {
+          val box = new HBox() {
+            alignment = Pos.CenterLeft
+            children = Seq(
+              labels(i),
+              new Region() {
+                hgrow = Priority.Always
+              },
+              sliders(i),
+              new Region() {
+                hgrow = Priority.Always
+              },
+              fields(i)
+            )
+          }
+          box
+        }
+      )
+    }
+
+    // Create toggles
+    val rgbToggle = new ToggleButton("RGB") {
+      id = "RGB"
+    }
+    val hsbToggle = new ToggleButton("HSB") {
+      id = "HSB"
+    }
+    val webToggle = new ToggleButton("Web") {
+      id = "WEB"
+    }
+
+    // Create toggle group
+    val toggleGroup = new ToggleGroup()
+    toggleGroup.toggles = Seq(rgbToggle, hsbToggle, webToggle)
+    toggleGroup.selectToggle(rgbToggle)
+    toggleGroup.selectedToggle.onChange {
+      val selected = toggleGroup.selectedToggle.get.asInstanceOf[javafx.scene.control.ToggleButton]
+      selected.id() match {
+        case a if a == "RGB" =>
+          set(0, "Red:", 255, red)
+          set(1, "Green:", 255, green)
+          set(2, "Blue:", 255, blue)
+        case b if b == "HSB" =>
+          set(0, "Hue:", 360, hue)
+          set(1, "Saturation:", 100, sat)
+          set(2, "Brightness:", 100, bright)
+        case c if c == "WEB" =>
+      }
+    }
+
+    def set(i: Int, label: String, max: Int, property: DoubleProperty): Unit = {
+      labels(i).setText(label)
+      sliders(i).setMax(max)
+    }
+
+    children = Seq(
+      new HBox() {
+        alignment = Pos.Center
+        children = Seq(rgbToggle, hsbToggle, webToggle)
+      },
+      settingsPane
+    )
+  }
+
   private val colorPalette = new GridPane() {
     vgrow = Priority.Never
     hgap = 1
@@ -195,12 +448,12 @@ class ColorChooser extends VBox {
       } yield swatch
       swatches
     }
-    buttonProp.value = { val button = children.head.asInstanceOf[jfxc.Button]; button }
+    buttonProp.value = children.head.asInstanceOf[jfxc.Button]
   }
 
   private val box = new VBox() {
     styleClass.add("color-rect-pane")
-    children = Seq(colorBar, colorRect, previewRect, colorPalette)
+    children = Seq(colorBar, colorRect, previewRect, colorControl, colorPalette)
   }
 
   this.children = Seq(box)
